d68f7a5f60f4c911a54fc6b3f3a0e79cc275c148
qAqa1 <aleksei062@mail.ru>
2024-07-13 00:14:19 +0300

    Face swap core

--- faceSwapApi/core/face_swap_core.py
+++ faceSwapApi/core/face_swap_core.py
@@ -19,7 +19,36 @@ swapper = insightface.model_zoo.get_model('core/inswapper_128.onnx',
 
 
 def detect_faces(cv_image):
-    faces = app.get(cv_image)
+    return app.get(cv_image)
+
+
+def get_faces_rectangles(faces):
     face_boxes = list()
     [face_boxes.append(face['bbox']) for face in faces]
     return face_boxes
+
+
+def swap_face(body_image, source_face, target_face):
+    image = body_image.copy()
+    image = swapper.get(body_image, source_face, target_face, paste_back=True)
+    return image
+
+
+def swap_face_by_images(cv_image_source_body, cv_image_target_face, source_body_face_index = 0):
+    image = cv_image_source_body.copy()
+
+    source_faces = detect_faces(cv_image_source_body)
+
+    if len(source_faces) == 0:
+        raise ValueError("Source image don't contains any face")
+
+    if len(source_faces) - 1 < source_body_face_index:
+        raise IndexError("Face index out of range")
+        # raise ValueError("Face index out of range")
+
+    target_faces = detect_faces(cv_image_target_face)
+
+    if len(target_faces) == 0:
+        raise ValueError("Target face-image don't contains any face")
+
+    return swap_face(cv_image_source_body, source_faces[source_body_face_index], target_faces[0])
--- faceSwapApi/utility/utility.py
+++ faceSwapApi/utility/utility.py
@@ -2,6 +2,8 @@ import cv2
 
 
 def draw_rectangle_with_text(cv_image, np_array_rectangle, cv_rectangle_color, label, cv_label_color):
+    image = cv_image.copy()
+
     x1 = int(np_array_rectangle[0])
     y1 = int(np_array_rectangle[1])
 
@@ -9,7 +11,7 @@ def draw_rectangle_with_text(cv_image, np_array_rectangle, cv_rectangle_color, l
     y2 = int(np_array_rectangle[3])
 
     # For bounding box
-    img = cv2.rectangle(cv_image, (x1, y1), (x2, y2), cv_rectangle_color, 2)
+    cv2.rectangle(image, (x1, y1), (x2, y2), cv_rectangle_color, 2)
 
     # For the text background
     # Finds space required by the text so that we can put a background with that amount of width.
@@ -17,18 +19,19 @@ def draw_rectangle_with_text(cv_image, np_array_rectangle, cv_rectangle_color, l
         label, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
 
     # Prints the text.
-    img = cv2.rectangle(img, (x1, y1 - 20), (x1 + w, y1), cv_rectangle_color, -1)
-    img = cv2.putText(img, label, (x1, y1 - 5),
+    image = cv2.rectangle(image, (x1, y1 - 20), (x1 + w, y1), cv_rectangle_color, -1)
+    image = cv2.putText(image, label, (x1, y1 - 5),
                       cv2.FONT_HERSHEY_SIMPLEX, 0.6, cv_label_color, 1)
-    return img
+    return image
 
 
 def draw_rectangles_with_text(cv_image, np_array_rectangles, label):
-    colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 0, 255), (255, 255, 0), (0, 255, 255)]
+    image = cv_image.copy()
+    colors = [(0, 0, 255), (0, 255, 0), (255, 0, 0), (255, 0, 255), (255, 255, 0), (0, 255, 255)]
     for index, rectangle in enumerate(np_array_rectangles):
         rectangle_color = colors[index % len(colors)]
         label_color = (0, 0, 0)
-        cv_image = draw_rectangle_with_text(cv_image, rectangle, rectangle_color, 'face ' + str(index),
+        image = draw_rectangle_with_text(image, rectangle, rectangle_color, 'face ' + str(index),
                                             label_color)
-    return cv_image
+    return image
 
